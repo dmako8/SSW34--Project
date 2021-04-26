8e87345b1a2a9c6e38cea46d22b77d36e6ad3e88
vmysh <73319790+vmysh@users.noreply.github.com>
2021-04-25 22:24:18 -0400

    added .env .gitignore
    need to add Discord bot key

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const Discord = require('discord.js');
+require('dotenv').config();
 const client = new Discord.Client();
-client.login('BOT TOKEN'); //create a separate configuration file later for the token
+client.login(bot_token); //create a separate configuration file later for the token
+const bot_token = process.env.DISCORD_KEY;
+
+//console.log(process.env);
 
 client.on('ready', readyDiscord);
 
