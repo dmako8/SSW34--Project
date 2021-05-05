cfde0d4daa8881fb5c9a54e233f809ef02fa4b9a
vmysh <73319790+vmysh@users.noreply.github.com>
2021-05-05 01:55:16 -0400

    working on fetching crypto data
    still in progress

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fetch = require('node-fetch');
 const Discord = require('discord.js');
 require('dotenv').config();
 const client = new Discord.Client();
@@ -13,10 +14,19 @@ function readyDiscord(){
 
 client.on('message', gotMessage);
 
-function gotMessage(msg) {
+async function gotMessage(msg) {
     //console.log(msg.content);
-    if (msg.channel.id == '839310571161255936' && msg.content === 'hello'){
+    if (msg.channel.id == '839310571161255936'){
+        if (msg.content === 'hello'){
         //msg.reply('how\'s it going?');
         msg.channel.send('how\'s it going?');
+        }
+        else if (msg.content == '!price') {
+            msg.channel.send('here\'s the price:');
+            let url = `https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=CNY&apikey=${process.env.VANTAGEAPI}`
+            let response = await fetch(url);
+            let json = await response.json();
+            console.log(json);
+        }
     }
 }
